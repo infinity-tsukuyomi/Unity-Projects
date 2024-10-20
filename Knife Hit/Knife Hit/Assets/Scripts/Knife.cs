fb9ab5cda0126461483c08f43baa319c3555b9a7
Infinite Tsukuyomi <misha.tyupyshev@gmail.com>
2024-10-20 19:14:41 +0300

    Knife Hit
    New Game

---
+++ Knife Hit/Knife Hit/Assets/Scripts/Knife.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knife : MonoBehaviour {
+
+	public float speed = 5f;
+	private Rigidbody myBody;
+	private bool onWood;
+
+	private KnifeSpawner knifeSpawner;
+
+	void Awake () {
+		myBody = GetComponent<Rigidbody>();
+
+		knifeSpawner = GameObject.Find("Knife Spawner").GetComponent<KnifeSpawner>();
+	}
+	
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.Space) && !onWood) {
+			myBody.velocity = new Vector3(0f, speed, 0f);
+		}
+
+	}
+
+	void OnTriggerEnter (Collider target) {
+		if (target.tag == "Wood") {
+			gameObject.transform.SetParent(target.transform); // we get wood and knife in the same parent(to rotate them together)
+			myBody.velocity = Vector3.zero; // stop the knife
+			onWood = true;
+			myBody.detectCollisions = false; // not detect collision anymore
+
+			knifeSpawner.SpawnKnife();
+			knifeSpawner.IncrementScore();
+		}
+
+		if (target.tag == "Knife") {
+			UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+		}
+	}
+}
